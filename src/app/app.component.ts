3bc5917f017ba1f1c6c5402b890bd950f113853f
Anirban <anirbandali247@gmail.com>
2020-03-11 22:15:40 -0700

    qna

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+interface Question{
+  questionId:number;
+  questionText:string;
+  options: Array<Options>;
+}
+interface Options{
+  optionId:number;
+  optionText:string;
+ 
 
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Demo';
+  questionList:Question[]=[
+    
+    {questionId:1,questionText:'Question 1',options:[{optionId:1,optionText:'Yes'},{optionId:2,optionText:'No'}]},
+    {questionId:2,questionText:'Question 2',options:[{optionId:1,optionText:'Yes'},{optionId:2,optionText:'No'},{optionId:3,optionText:'May be'}]},
+    {questionId:3,questionText:'Question 3',options:[{optionId:1,optionText:'Yes'},{optionId:2,optionText:'No'}]},
+    {questionId:4,questionText:'Question 4',options:[{optionId:1,optionText:'Yes'},{optionId:2,optionText:'No'},{optionId:3,optionText:'May be'}]},
+    {questionId:5,questionText:'Question 5',options:[{optionId:1,optionText:'Yes'},{optionId:2,optionText:'No'}]}
+
+  
+  
+  ]
+  filteredQuestionList: Question[];
+  filteredOption=[];
+  OnRule(question){
+    console.log(question);
+ 
+    let filtered=this.questionList.filter(_=>_.questionId!==question.questionId);
+    this.filteredQuestionList=filtered.filter(_=>_.questionId<question.questionId)
+  }
+
+  OnSelect(questionId){
+   
+   let filt:Question[]=this.filteredQuestionList.filter(_=>_.questionId==questionId)
+
+  this.filteredOption=filt[0].options;
+
+
+  }
 }
